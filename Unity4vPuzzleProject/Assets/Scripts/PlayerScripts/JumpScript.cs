1a973dc41063f4e37a1d7f019ff515fce4d3595f
isahut <87544791+isahut@users.noreply.github.com>
2021-07-20 12:07:47 -0700

    changes I had to commit

--- Unity4vPuzzleProject/Assets/Scripts/PlayerScripts/JumpScript.cs
+++ Unity4vPuzzleProject/Assets/Scripts/PlayerScripts/JumpScript.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class JumpScript : MonoBehaviour
 {
 
-    float forceCount = 0f;
-    bool jumpingUp;
+ 
    
     // Start is called before the first frame update
     void Start()
