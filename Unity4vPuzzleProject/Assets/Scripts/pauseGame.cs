fc95fb78b33622cc3c33db437084f3e60bdc29ba
KonaB15 <80014438+KonaB15@users.noreply.github.com>
2021-07-21 12:06:00 -0600

    Stuff

---
+++ Unity4vPuzzleProject/Assets/Scripts/pauseGame.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseGame : MonoBehaviour
+{
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
